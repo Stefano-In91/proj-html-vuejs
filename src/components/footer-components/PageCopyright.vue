e058852e3d1659172f6e720880f92c006d59a7c0
Stefano.In91 <ste.ince91@gmail.com>
2022-12-06 11:18:25 +0100

    adds copyright section

--- src/components/footer-components/PageCopyright.vue
+++ src/components/footer-components/PageCopyright.vue
@@ -1,11 +1,29 @@
 <script>
 export default {
   name: "PageCopyright",
+  data() {
+    return {
+      copyright: "© Copyright 2012-2020",
+      theme: "Avada Theme by InceFusion",
+      rights: "All Rights Reserved",
+      powered_by: "Powered by WordPress",
+    };
+  },
 };
 </script>
 
 <template>
-  <h1>PageCopyright</h1>
+  <section id="copyright" class="text-center">
+    <p>{{ copyright }} | {{ theme }} | {{ rights }} | {{ powered_by }}</p>
+  </section>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+#copyright {
+  padding: 3rem 0;
+  background-color: var(--copyright-color);
+  p {
+    color: rgba(169, 169, 169, 0.185);
+  }
+}
+</style>
