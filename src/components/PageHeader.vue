58e0b15792f2dadee6f8a75bfa138b0a5763ee4a
Stefano.In91 <ste.ince91@gmail.com>
2022-12-09 11:24:00 +0100

    adds navbar display on click

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -1,20 +1,36 @@
 <script>
+import NavMenu from "./main-components/NavMenu.vue";
+
 export default {
   name: "PageHeader",
+  components: {
+    NavMenu,
+  },
+  data() {
+    return {
+      showNav: false,
+    };
+  },
+  methods: {
+    toggleNavbar() {
+      this.showNav = !this.showNav;
+    },
+  },
 };
 </script>
 
 <template>
-  <header>
+  <header :class="showNav ? `navActive` : ``">
     <div class="container">
       <div class="flex-between-center">
         <img src="../assets/img/avadabarbers-logo-x1.png" alt="logo" />
         <div class="icons flex-center">
           <a><i class="fa-solid fa-cart-shopping"></i></a>
-          <a><i class="fa-solid fa-bars"></i></a>
+          <a @click="toggleNavbar"><i class="fa-solid fa-bars"></i></a>
         </div>
       </div>
     </div>
+    <NavMenu :display="showNav" @hide="toggleNavbar" />
   </header>
 </template>
 
@@ -24,6 +40,9 @@ header {
   z-index: 99;
   padding: 1rem 0;
   background-color: inherit;
+  &.navActive {
+    background-color: var(--darkbg-color);
+  }
   .icons {
     color: white;
     height: 100%;
---
+++ src/components/main-components/NavMenu.vue
@@ -0,0 +1,47 @@
+<script>
+import links from "../../assets/json/nav-items.json";
+
+export default {
+  name: "NavMenu",
+  props: {
+    display: Boolean,
+  },
+  data() {
+    return {
+      links,
+    };
+  },
+};
+</script>
+<template>
+  <nav class="text-center" :class="display ? `active` : ``">
+    <ul>
+      <li v-for="link in links">
+        <a :href="link.link" @click="$emit(`hide`)">{{ link.name }}</a>
+      </li>
+    </ul>
+  </nav>
+</template>
+<style lang="scss" scpoped>
+nav {
+  display: none;
+  background-color: var(--darkbg-color);
+  color: white;
+  position: relative;
+  width: 100%;
+  top: 0;
+  padding: 3rem 0;
+  &.active {
+    display: block;
+  }
+  li {
+    font-size: 1.25rem;
+    &:not(:last-of-type) {
+      margin-bottom: 1rem;
+    }
+    &:hover {
+      color: var(--gold-color);
+    }
+  }
+}
+</style>
